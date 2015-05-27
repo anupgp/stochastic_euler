1a2005d2b4fd7dd6002e087ab4515ed08d744642
Pranav Kulkarni <pranavcode@gmail.com>
2015-05-28 02:10:43 +0530

    Improve code readability and overall quality

--- det_model_hh_post.hpp
+++ det_model_hh_post.hpp
@@ -1,11 +1,8 @@
-/* Last Revised Date: 13_DEC_2014 */
-/* Lastest status: All ok    */
-
 /*
   det_model_hh_post.hpp : implimentation of HH system (similar but not exact for the classical HH equations)  
-  !!!NOTE: all units are in SI system
 
-  Copyright (C) 2015 Anup Gopalakrishna Pillai, Suhita Nadkarni Lab, IISER Pune <anupgpillai@gmail.com>
+  Copyright (C) 2015 Anup Gopalakrishna Pillai, Suhita Nadkarni Lab, IISER Pune <anupgpillai@gmail.com> 
+  Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -29,59 +26,61 @@
 #include <cmath>
 #include <boost/math/constants/constants.hpp>
 
-//UNITS: all SI units - seconds, Volts, Ampere, Meters, Simenes, Farads
-
-//DECLARATIONS FOR HH_POST_MODEL
-
-class DET_MODEL_HH_POST
+/*
+ * A note on units in this code:
+ * All SI units - Seconds, Volts, Amperes, Meters, Simenes, Farads
+ */
+class det_model_hh_post
 {
-private:
+ private:
   const double pi = boost::math::constants::pi<double>();
-  double Cm = 1.0E-02;   // F/m^2 Specific capacitance
-  //----Reversal potentials for Na, K, channels in mV
+  double Cm = 1.0E-02;     // F/m^2 Specific capacitance
   double ENa = 50.0E-03;   // Reversal potential for Sodium currents in Volts
-  double EK = -85.0E-03;    // Reversal potential for Potassium currents in Volts
-  double ELeak = -81E-03; // Reversal potential for Leak currents in Volts
-
-  double GNa = 130E01;   // Maximum conductance for Sodium channels S/m^2
-  double GK = 36E01;    // Maximum conductance for Potassium channels S/m^2
-  double GLeak = 0.3E01;  // Maximum conductance for Leak channels S/m^2
-
-public:
+  double EK = -85.0E-03;   // Reversal potential for Potassium currents in Volts
+  double ELeak = -81E-03;  // Reversal potential for Leak currents in Volts
+  double GNa = 130E01;     // Maximum conductance for Sodium channels S/m^2
+  double GK = 36E01;       // Maximum conductance for Potassium channels S/m^2
+  double GLeak = 0.3E01;   // Maximum conductance for Leak channels S/m^2
+ public:
   std::vector<double> X;
 
-  double IExt = 0.0; //extern current in Amps
+  double IExt = 0.0;       // external current in Amps
   double Area = 0.0;
   double Cap = 0.0;
-  double V = 0.0;      // Membrane voltage V
+  double V = 0.0;          // Membrane voltage V
+
   /* NEURON class explicit constructor */
-  explicit DET_MODEL_HH_POST( std::vector<double> X_ = {0,0,0,0},double Area_ = 8.5E-12): Area(Area_), X(X_), Cap(Cm * Area) // Area in m^2
-  {
-  };
-  //---Equations for bouton Na Channel (Ref: Dominique Engel & Peter Jonas, 2005, Neuron)
+  explicit det_model_hh_post(std::vector<double> X_ = {0,0,0,0},double Area_ = 8.5E-12):
+      Area(Area_), X(X_), Cap(Cm * Area) {}; // Area in m^2
+
+  // Equations for bouton Na Channel (Ref: Dominique Engel & Peter Jonas, 2005, Neuron)
   double alpha_Na_act(const double V);
   double beta_Na_act(const double V);
   double alpha_Na_inact(const double V);
   double beta_Na_inact(const double V);
-  //---Equations for bouton K Channel (Ref: Dominique Engel & Peter Jonas, 2005, Neuron)
+
+  // Equations for bouton K Channel (Ref: Dominique Engel & Peter Jonas, 2005, Neuron)
   double alpha_K_act(const double V);
   double beta_K_act(const double V);
-  //---Declaration of the ODE solver
-  template <class State >
-  void operator() ( const State &x, State &dxdt , const double  t );
+
+  // Declaration of the ODE solver
+  template<class State>
+  void operator()(const State &x, State &dxdt, const double t);
 };
-//Function declarations
-//------------- Na+ channels --- implimented from classical HH (1952)
-double DET_MODEL_HH_POST::alpha_Na_act(const double V)
+
+// Na+ channels -- Implemented from classical HH model (1952)
+double det_model_hh_post::alpha_Na_act(const double V)
 {
   double a = 0.1E03;
   double b = 45.33E-03;
   double c = 10E-03;
   double val = 0.0;
+
   val = (- a * ( (V + b) / (exp( -(V + b) / c ) - 1 ) ) );
   return (val);
 }
-double DET_MODEL_HH_POST::beta_Na_act(const double V)
+
+double det_model_hh_post::beta_Na_act(const double V)
 {
   double a = 4;
   double b = 70.33E-03;
@@ -90,43 +89,51 @@ double DET_MODEL_HH_POST::beta_Na_act(const double V)
   val = ( a * ( exp( - (V + b) / c )) );
   return (val);
 }
-double DET_MODEL_HH_POST::alpha_Na_inact(const double V)
+
+double det_model_hh_post::alpha_Na_inact(const double V)
 {
   double val = 0.0;
   val = 0.07 * exp( -( V + 70.33E-03) /20E-03 );
   return (val);
 }
-double DET_MODEL_HH_POST::beta_Na_inact(const double V)
+
+double det_model_hh_post::beta_Na_inact(const double V)
 {
   double val = 0.0;
   val = ( 1 / ( exp( - (V + 40.33E-03) / 10E-03) + 1) );
   return (val);
 }
-//--------K+ channel alpha & beta
-double DET_MODEL_HH_POST::alpha_K_act(const double V)
+
+// K+ channel alpha & beta
+double det_model_hh_post::alpha_K_act(const double V)
 {
   double val = 0;
   val = -0.01E03 * (V + 60.33E-03) / ( exp( -(V +  60.33E-03) / 10E-03) - 1 );
   return (val);
-};
-double DET_MODEL_HH_POST::beta_K_act(const double V)
+}
+
+double det_model_hh_post::beta_K_act(const double V)
 {
   double val = 0.0;
   val = 0.125 * exp (- (V + 70.33E-03) / 80E-03);
   return (val);
-};
-//-------HH_PRE class ODE Function
+}
 
+// HH_PRE class ODE Function
 template <class State>
-void DET_MODEL_HH_POST::operator() ( const State &x, State &dxdt , const double t )
+void det_model_hh_post::operator()(const State &x, State &dxdt, const double t)
 {
+  // Na activation
+  dxdt[0] = 1E03 * ((alpha_Na_act(x[3]) * (1 - x[0])) - (beta_Na_act(x[3]) * x[0]) );
+  dxdt[1] = 1E03 * ((alpha_Na_inact(x[3]) * (1 - x[1])) - (beta_Na_inact(x[3]) * x[1]));
+  // K activation
+  dxdt[2] = 1E03 * ((alpha_K_act(x[3]) * (1 - x[2])) - (beta_K_act(x[3]) * x[2]));
 
-  dxdt[0] = 1E03 * (( alpha_Na_act(x[3]) * (1 - x[0]) ) - (beta_Na_act(x[3]) * x[0]) ); // Na activation
-  dxdt[1] = 1E03 * ( ( alpha_Na_inact(x[3]) * (1 - x[1]) ) - (beta_Na_inact(x[3]) * x[1])) ; // Na inactivation
-  dxdt[2] = 1E03 * ( ( alpha_K_act(x[3]) * (1 - x[2]) ) - (beta_K_act(x[3]) * x[2])); // K activation
-  double INa =   -(Area * GNa * pow(x[0],3) * x[1] * (x[3] - ENa) );
-  double IK =   -(Area * GK * pow(x[2],4) * (x[3] - EK) );
+  double INa = -(Area * GNa * pow(x[0],3) * x[1] * (x[3] - ENa) );
+  double IK = -(Area * GK * pow(x[2],4) * (x[3] - EK) );
   double ILeak = -(Area * GLeak *  (x[3] - ELeak) );
-  dxdt[3] =  (1/Cap) * (INa + IK + ILeak  + (IExt * Area)  );
-};
-#endif // DET_MODEL_HH_POST_HPP_INCLUDED
+
+  dxdt[3] =  (1/Cap) * (INa + IK + ILeak  + (IExt * Area));
+}
+
+#endif
--- stoch_odeint_explicit_euler.hpp
+++ stoch_odeint_explicit_euler.hpp
@@ -2,6 +2,7 @@
   stoch_odeint_explicit_euler.hpp : stochastic Euler implimentaion header
 
   Copyright (C) 2015 Anup Gopalakrishna Pillai, Suhita Nadkarni Lab, IISER, Pune <anupgpillai@gmail.com>
+  Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -17,7 +18,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #ifndef STOCH_ODEINT_EXPLICIT_EULER_HPP_INCLUDED
 #define STOCH_ODEINT_EXPLICIT_EULER_HPP_INCLUDED
 
@@ -30,63 +30,67 @@
 
 namespace stoch_odeint_explicit_euler {
 
-  template <class state_type>  
-  class STOCH_ODEINT_EXPLICIT_EULER
-  {
-  private:
-    long long seed;
-    BOOST_RNG rand;
-  public:
-    
-    STOCH_ODEINT_EXPLICIT_EULER(long long seed_ = std::time(0) +  (long long) getpid() ):seed(seed_), rand(seed) { };
-    template < class system_type >
-    void do_step(system_type &system, state_type &x, const double t, const double dt,  std::vector<unsigned> &s); 
-  };
-  template <class state_type> 
-  template <class system_type>
-  void STOCH_ODEINT_EXPLICIT_EULER<state_type>::do_step(system_type &system, state_type &x, const double t, const double dt, std::vector<unsigned> &s )
-  {
-    state_type d (x.size(),0.0);
-    system(x,d,t); 		// obtain the derivative at x
-    typename state_type::iterator xi = x.begin();
-    typename state_type::iterator di = d.begin();
-    std::vector<unsigned>::iterator si = s.begin();
-    for(int i=0; i<x.size(); i++) {
-      *(xi) = (*xi) + (dt *  (*di))  +  ( pow(dt,0.5) * rand.norm_double() * (*si)  * (*xi))  ; // Euler-Murayama algorithm
-      ++xi;
-      ++di;
-      ++si;
-    }
-  };
-//  End of stoch_odeint_explicit_euler namespace
+template<class state_type>  
+class stoch_odeint_explicit_euler
+{
+ private:
+  long long seed;
+  boost_rng rand;
+ public:
+  stoch_odeint_explicit_euler(long long seed_ = std::time(0) + (long long)getpid()):
+      seed(seed_), rand(seed) {};
+  template<class system_type>
+  void do_step(system_type &system, state_type &x, const double t, const double dt,  std::vector<unsigned> &s); 
+};
+
+template<class state_type> 
+template<class system_type>
+void stoch_odeint_explicit_euler<state_type>::do_step(system_type &system, state_type &x, const double t, const double dt, std::vector<unsigned> &s )
+{
+  state_type d (x.size(),0.0);
+  system(x,d,t); 		// obtain the derivative at x
+  auto xi = x.begin();
+  auto di = d.begin();
+  auto si = s.begin();  
+  for(auto i : x) {  // Euler-Murayama algorithm
+    *(xi) = (*xi) + (dt * (*di)) + (pow(dt,0.5) * rand.norm_double() * (*si)  * (*xi));
+    ++xi; ++di; ++si;
+  }
 }
+
+} // namespace stoch_odeint_explicit_euler
 
 // An observer that writes the time and state vector into stdout 
 struct null_observer 
 {
-  template < class state_type>
+  template<class state_type>
   void operator()(const state_type &x, double t) const
   {
-    std::cout << t << " " << x << std::endl;  
+    std::cout << t << " " << x << std::endl;
   }
 };
 
 // integrate function with observer object
-template <class stepper_type, class system_type, class state_type, class observer_type>
-void integrate_const(stepper_type stepper, system_type system, state_type &x, const double tstart, const double tend, const double dt, std::vector<unsigned> &s, observer_type observer)
+template<class stepper_type, class system_type, class state_type, class observer_type>
+void integrate_const(stepper_type stepper, system_type system,
+                     state_type &x, const double tstart, const double tend,
+                     const double dt, std::vector<unsigned> &s, observer_type observer)
 {
-  for(double t = tstart; t <= tend; t+=dt){
+  for(double t = tstart; t <= tend; t += dt) {
     stepper.do_step(system,x,t,dt,s);
     observer(x,t);
   }
-};
+}
 
 // integrate function without observer object
-template <class stepper_type, class system_type, class state_type>
-void integrate_const(stepper_type stepper, system_type system, state_type &x, const double tstart, const double tend, const double dt, std::vector<unsigned> &s)
+template<class stepper_type, class system_type, class state_type>
+void integrate_const(stepper_type stepper, system_type system,
+                     state_type &x, const double tstart, const double tend,
+                     const double dt, std::vector<unsigned> &s)
 {
   for(double t = tstart; t <= tend; t+=dt){
     stepper.do_step(system,x,t,dt,s);
   }
-};
-#endif // STOCH_ODEINT_EXPLICIT_EULER_HPP_INCLUDED
+}
+
+#endif
